1c218ffe25d0d068bfc134236d982e86b7424ef5
allenc66 <allenc6609@gmail.com>
2021-11-27 01:36:08 -0500

    Exercise1.1 1.2

---
+++ part1/part1/src/Components/Total.js
@@ -0,0 +1,7 @@
+const Total = (props) => {
+    const exercises = props.exercises
+    return (
+        <p> Number of exercises {exercises[0] + exercises[1] + exercises[2]} </p>
+    )
+}
+export {Total}
