ca89fe36083483e9bd4e48eb4b437e731fed84b0
allenc66 <allenc6609@gmail.com>
2021-11-29 15:43:14 -0500

    Update App.js
    exercise 1.3 - 1.5

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -4,11 +4,12 @@ import { Content } from './Components/Content'
 import { Total } from './Components/Total'
 
 const App = (props) => {
-  const course = 'Half Stack application development'
-
-  parts = [{name = 'Fundamentals of React', exercises = 10}, 
-  {name = 'Using props to pass data', exercises = 7}, 
-  {name = 'State of a component', exercises = 14}]
+  const course = { 
+    name: 'Half Stack application development', 
+    parts: [{name = 'Fundamentals of React', exercises = 10}, 
+            {name = 'Using props to pass data', exercises = 7}, 
+            {name = 'State of a component', exercises = 14}] 
+  }
   
 
   return (
