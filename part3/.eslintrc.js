ea46ea960670c8fae79d7c96d80626203ed23b7d
allenc66 <allenc6609@gmail.com>
2022-02-09 17:14:49 -0500

    eslintrc update

--- part3/.eslintrc.js
+++ part3/.eslintrc.js
@@ -24,6 +24,19 @@ module.exports = {
         'semi': [
             'error',
             'never'
-        ]
+        ],
+        'eqeqeq': 'error',
+
+        'no-trailing-spaces': 'error',
+
+        'object-curly-spacing': [
+        'error', 'always'
+        ],
+
+        'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+        ],
+
+        'no-console': 0
     }
 }
